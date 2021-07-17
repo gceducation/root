92c225c54f91dc453cd6765b3089a13fc7e0f779
Education at Gadre Consulting <education@gadreconsulting.com>
2021-07-17 20:50:30 +0530

    more programs

---
+++ CSharp/TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        int _jNumOfCells = 3;
+        int _jCellSize = 100;
+        byte _bCurMove = Game._bBitsCross;
+        Game _game = new Game();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Pen pen = Pens.Black;
+            Graphics g = e.Graphics;
+            for (int j = 0; j < _jNumOfCells + 1; j++)
+            {
+                int jX1 = j * _jCellSize;
+                int jY1 = 0;
+                int jX2 = jX1;
+                int jY2 = _jNumOfCells * _jCellSize;
+                g.DrawLine(pen, jX1, jY1, jX2, jY2);
+                jX1 = 0;
+                jY1 = j * _jCellSize;
+                jX2 = _jNumOfCells * _jCellSize;
+                jY2 = jY1;
+                g.DrawLine(pen, jX1, jY1, jX2, jY2);
+            }
+
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs mouea = e as MouseEventArgs;
+            Game.XY xy = FindCellFromXY(mouea.X, mouea.Y);
+            if (_game.BGetMark(xy._jx, xy._jy) == Game._bBitsEmpty)
+            {
+                DrawInBox(xy._jx, xy._jy);
+                int jVal = _game.ComputeNextMove(out xy);
+                if (xy != null)
+                {
+                    DrawInBox(xy._jx, xy._jy);
+                }
+                int nEmpty;
+                Game.EGAMESTATE ega = _game.GetGameState(out nEmpty);
+                String sMsg = null;
+                switch (ega)
+                {
+                    case Game.EGAMESTATE.eDraw:
+                        sMsg = "It is a tie!\r\nPlay Again?";
+                        break;
+                    case Game.EGAMESTATE.eWinnerNought:
+                        sMsg = "Computer wins!\r\nPlay Again?";
+                        break;
+                    case Game.EGAMESTATE.eWinnerCross:
+                        throw new Exception("Computer cannot lose");
+                }
+                if (sMsg != null)
+                {
+                    DialogResult dlgres = MessageBox.Show(sMsg, "Game Over", MessageBoxButtons.YesNo);
+                    if (dlgres == DialogResult.Yes)
+                    {
+                        _game = new Game();
+                        _bCurMove = Game._bBitsCross;
+                        this.Refresh();
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Already Filled");
+            }
+        }
+
+        private void DrawInBox(int jx, int jy)
+        {
+            Graphics g = panel1.CreateGraphics();
+
+            if (_bCurMove == Game._bBitsCross)
+            {
+                g.DrawLine(Pens.Blue, jx * _jCellSize + _jCellSize / 10, jy * _jCellSize + _jCellSize / 10, (jx + 1) * _jCellSize - _jCellSize / 10, (jy + 1) * _jCellSize - _jCellSize / 10);
+                g.DrawLine(Pens.Blue, jx * _jCellSize + _jCellSize / 10, (jy + 1) * _jCellSize - _jCellSize / 10, (jx + 1) * _jCellSize - _jCellSize / 10, jy * _jCellSize + _jCellSize / 10);
+            }
+            else
+            {
+                g.DrawEllipse(Pens.Green, jx * _jCellSize + _jCellSize / 10, jy * _jCellSize + _jCellSize / 10,
+                    _jCellSize - _jCellSize / 5, _jCellSize - _jCellSize / 5);
+            }
+            _game.SetMark(jx, jy, _bCurMove);
+            int nEmpty;
+            Game.EGAMESTATE egs = _game.GetGameState(out nEmpty);
+            _bCurMove = (byte)((Game._bBitsCross | Game._bBitsNought) ^ _bCurMove);
+            g.Dispose();
+
+        }
+
+        private Game.XY FindCellFromXY(int jx, int jy)
+        {
+            Game.XY xyRet = new Game.XY();
+            xyRet._jx = jx / _jCellSize;
+            xyRet._jy = jy / _jCellSize;
+            return xyRet;
+        }
+    }
+}
---
+++ CSharp/TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        // for a board of 3x3 we need 18 bits.
+        byte[] _abBoard = new byte[3];
+
+        public static byte _bBitsEmpty = 0;
+        public static byte _bBitsCross = 1;
+        public static byte _bBitsNought = 2;
+
+
+
+        // two bits for each cell per row
+        public enum EGAMESTATE
+        {
+            eIncomplete,
+            eDraw,
+            eWinnerCross,
+            eWinnerNought
+        }
+        public Game()
+        {
+            int nEmpty;
+            GetGameState(out nEmpty);
+        }
+        public EGAMESTATE GetGameState(out int nEmpty)
+        {
+            EGAMESTATE estate = EGAMESTATE.eIncomplete;
+            nEmpty = 0;
+            int jx;
+            int jy;
+            byte bMark = _bBitsEmpty;
+            for (jx = 0; jx < 3; jx++)
+            {
+                for (jy = 0; jy < 3; jy++)
+                {
+                    if (BGetMark(jx, jy) == _bBitsEmpty)
+                    {
+                        nEmpty++;
+                    }
+                }
+            }
+
+            // Check Rows
+            for (jx = 0; jx < 3; jx++)
+            {
+                bMark = (byte)(Game._bBitsCross | Game._bBitsNought);
+                for (jy = 0; jy < 3; jy++)
+                {
+                    bMark &= BGetMark(jx, jy);
+
+                }
+                if (bMark == Game._bBitsCross)
+                {
+                    estate = EGAMESTATE.eWinnerCross;
+                    break;
+
+                }
+                else if (bMark == Game._bBitsNought)
+                {
+                    estate = EGAMESTATE.eWinnerNought;
+                    break;
+                }
+
+            }
+            if (estate == EGAMESTATE.eIncomplete)
+            {
+                // Check Columns
+                if (estate == EGAMESTATE.eIncomplete)
+                {
+                    for (jy = 0; jy < 3; jy++)
+                    {
+                        bMark = (byte)(Game._bBitsCross | Game._bBitsNought);
+                        for (jx = 0; jx < 3; jx++)
+                        {
+                            bMark &= BGetMark(jx, jy);
+
+                        }
+                        if (bMark == Game._bBitsCross)
+                        {
+                            estate = EGAMESTATE.eWinnerCross;
+                            break;
+
+                        }
+                        else if (bMark == Game._bBitsNought)
+                        {
+                            estate = EGAMESTATE.eWinnerNought;
+                            break;
+                        }
+                    }
+
+                }
+                // Check Diagonal 1
+                if (estate == EGAMESTATE.eIncomplete)
+                {
+                    bMark = (byte)(Game._bBitsCross | Game._bBitsNought);
+                    for (jx = 0; jx < 3; jx++)
+                    {
+
+                        bMark &= BGetMark(jx, jx);
+                    }
+                    if (bMark == Game._bBitsCross)
+                    {
+                        estate = EGAMESTATE.eWinnerCross;
+                    }
+                    else if (bMark == Game._bBitsNought)
+                    {
+                        estate = EGAMESTATE.eWinnerNought;
+                    }
+                }
+                // Check Diagonal 2
+                if (estate == EGAMESTATE.eIncomplete)
+                {
+                    bMark = (byte)(Game._bBitsCross | Game._bBitsNought);
+                    for (jx = 0; jx < 3; jx++)
+                    {
+
+                        bMark &= BGetMark(jx, 2 - jx);
+                    }
+                    if (bMark == Game._bBitsCross)
+                    {
+                        estate = EGAMESTATE.eWinnerCross;
+                    }
+                    else if (bMark == Game._bBitsNought)
+                    {
+                        estate = EGAMESTATE.eWinnerNought;
+                    }
+                }
+            }
+            if ((estate == EGAMESTATE.eIncomplete) && (nEmpty == 0))
+            {
+                estate = EGAMESTATE.eDraw;
+            }
+            return estate;
+        }
+
+
+        public byte BGetMark(int jx, int jy)
+        {
+            byte bRet = _bBitsEmpty;
+            // Create a mask
+            byte bMask = (byte)(0x3 << (jx * 2));
+            bRet = (byte)(_abBoard[jy] & bMask);
+            bRet >>= (jx * 2);
+            return bRet;
+        }
+        public void SetMark(int jx, int jy, byte bMark)
+        {
+            byte bVal;
+            // Create a mask
+            byte bMask = (byte)(0x3 << (jx * 2));
+            bVal = _abBoard[jy];
+            // Zero Out the bits
+            bVal &= (byte)(~(bMask));
+            bMask = (byte)(bMark << (jx * 2));
+            bVal |= bMask;
+            _abBoard[jy] = bVal;
+        }
+
+        public int ComputeNextMove(out XY xy)
+        {
+
+            return ComputeNextMoveInternal(null, out xy, _bBitsNought);
+        }
+        private const int _jGameWinValue = 333;
+        private int ComputeNextMoveInternal(XY xyCur, out XY xy, byte bPlayer)
+        {
+            int jVal = 0;
+            xy = xyCur;
+            int nEmpty;
+            EGAMESTATE egs = GetGameState(out nEmpty);
+            switch (egs)
+            {
+                case EGAMESTATE.eIncomplete:
+                    bool fFirstTime = true;
+                    XY xyBest = null;
+                    for (int jx = 0; jx < 3; jx++)
+                    {
+                        for (int jy = 0; jy < 3; jy++)
+                        {
+                            if (BGetMark(jx, jy) == _bBitsEmpty)
+                            {
+                                SetMark(jx, jy, bPlayer);
+                                byte bPlayerNextMove = (byte)((Game._bBitsCross | Game._bBitsNought) ^ bPlayer);
+                                XY xyTemp = new XY() { _jx = jx, _jy = jy };
+                                int jValT = -ComputeNextMoveInternal(xyTemp, out xy, bPlayerNextMove);
+                                if (fFirstTime || (jValT > jVal))
+                                {
+                                    jVal = jValT;
+                                    xyBest = xyTemp;
+                                    fFirstTime = false;
+                                }
+                                SetMark(jx, jy, _bBitsEmpty);
+                            }
+                        }
+                    }
+                    xy = xyBest;
+                    break;
+                case EGAMESTATE.eDraw:
+                    break;
+                case EGAMESTATE.eWinnerCross:
+                    jVal = bPlayer == _bBitsCross ? _jGameWinValue : -_jGameWinValue;
+                    break;
+                case EGAMESTATE.eWinnerNought:
+                    jVal = bPlayer == _bBitsNought ? _jGameWinValue : -_jGameWinValue;
+                    break;
+                default:
+                    throw new Exception("unexpected EGAMESTATE");
+            }
+
+
+
+            return jVal;
+        }
+        public class XY
+        {
+            public int _jx;
+            public int _jy;
+        }
+    }
+
+}

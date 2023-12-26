2856487f318ced157aa80d282896e6688e799e69
MAK-LAPTOP\gadre <makarand@gadreconsulting.com>
2023-12-26 21:46:28 +0530

    initial

---
+++ CSharp/RouteFinder/RouteFinder/RouteFinder/Airport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteFinder
+{
+    public class Airport
+    {
+        public String _sName;
+        public String _sIATACode;
+        public float _rLat;
+        public float _rLon;
+        public float _rAltitude;
+        public List<Airport> _lstAirportDep;
+        public List<Airport> _lstAirportArr;
+
+        public Airport(string sName, string sIATACode, float rLat, float rLon, float rAltitude, List<Airport> lstAirportDep, List<Airport> lstAirportArr)
+        {
+            _sName = sName;
+            _sIATACode = sIATACode;
+            _rLat = rLat;
+            _rLon = rLon;
+            _rAltitude = rAltitude;
+            _lstAirportDep = lstAirportDep;
+            _lstAirportArr = lstAirportArr;
+        }
+    }
+}
---
+++ CSharp/RouteFinder/RouteFinder/RouteFinder/Runner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteFinder
+{
+    public class Runner
+    {
+        public static void Main(String [] args)
+        {
+            Console.WriteLine("Start");
+            new Runner().Execute();
+        }
+        public void Execute()
+        {
+            
+        }
+    }
+}
